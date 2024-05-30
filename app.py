7bb47204c833a9391f29c3986a163b01df50a3d0
arulgundam <arul.gundam@gmail.com>
2024-05-30 16:28:45 -0400

    Database migration complete! Need to change script.js to reflect new desired hover message.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import pandas as pd
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ipl_data.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Match(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    season = db.Column(db.String(255))
+    match_id = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    home_team = db.Column(db.String(255))
+    away_team = db.Column(db.String(255))
     first_innings_score = db.Column(db.String(255))
     second_innings_score = db.Column(db.String(255))
+    result = db.Column(db.String(255))
 
 def load_data_to_db(csv_file):
     df = pd.read_csv(csv_file)
@@ -19,8 +27,14 @@ def load_data_to_db(csv_file):
     db.create_all()
 
     for match in matches_data:
-        db.session.add(Match(first_innings_score=match['1st_inning_score'], 
-                             second_innings_score=match['2nd_inning_score']))
+        db.session.add(Match(season=match['season'],
+                             match_id=match['id'],
+                             description=match['description'],
+                             home_team=match['home_team'],
+                             away_team=match['away_team'],
+                             first_innings_score=match['1st_inning_score'], 
+                             second_innings_score=match['2nd_inning_score'],
+                             result=match['result']))
     db.session.commit()
 
 
@@ -35,9 +49,17 @@ def index():
 @app.route('/data')
 def data():
     matches = Match.query.all()
-    ipl_data = [{"first_innings_score": match.first_innings_score, 
-                  "second_innings_score": match.second_innings_score} 
-                 for match in matches]
+    ipl_data = [{
+                "season": match.season, 
+                "match_id": match.match_id, 
+                "description": match.description, 
+                "home_team": match.home_team, 
+                "away_team": match.away_team, 
+                "first_innings_score": match.first_innings_score, 
+                "second_innings_score": match.second_innings_score,
+                "result": match.result, 
+                } 
+            for match in matches]
     return jsonify(ipl_data)
 
 if __name__ == '__main__':
